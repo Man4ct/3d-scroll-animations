7d7dc536421da47e6907b0fd444ac1cc318dbefb
Fahmi Fahreza <fahmiiireza@gmail.com>
2024-01-09 11:36:27 +0100

    change styling of the overlay

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,6 @@
 import { OrbitControls, ScrollControls } from '@react-three/drei';
 import { Office } from './Office';
+import {Overlay} from './Overlay';
 
 const Experience = () => {
   return (
@@ -8,6 +9,7 @@ const Experience = () => {
 
       <OrbitControls enableZoom={false}/>
       <ScrollControls pages={3} damping={0.25}>
+        <Overlay />
       <Office />
       </ScrollControls>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      serif: ["Playfair Display", ...defaultTheme.fontFamily.sans],
+      sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+    },
   },
   plugins: [],
-}
-
+};
